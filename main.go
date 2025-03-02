53920c0393b240d538e56f1343699513d56b7535
OminousShadow87 <brucewaynephil@gmail.com>
2025-03-02 14:44:05 -0800

    created more REPL interactivty

--- main.go
+++ main.go
@@ -8,15 +8,10 @@ import (
 )
 
 func main() {
-	scanner := bufio.NewScanner(os.Stdin)
-	for {
-		fmt.Print("Pokedex > ")
-		scanner.Scan()
-		input := scanner.Text()
-		result := parseFirstWord(input)
-		fmt.Println("Your command was:", result)
-	}
+	startRepl(commands)
 }
+
+var commands map[string]cliCommand
 
 func cleanInput(text string) []string {
 	lowered := strings.ToLower(text)
@@ -31,4 +26,57 @@ func parseFirstWord(text string) string {
 		return ""
 	}
 	return cleaned[0]
+}
+
+func commandExit() error {
+	fmt.Println("Closing the Pokedex... Goodbye!")
+	os.Exit(0)
+	return nil
+}
+
+func commandHelp() error {
+	fmt.Println("Welcome to the Pokedex!\nUsage:\n")
+	for name, cmd := range commands {
+		fmt.Printf("%s: %s\n", name, cmd.description)
+	}
+	return nil
+}
+
+type cliCommand struct {
+	name        string
+	description string
+	callback    func() error
+}
+
+func init() {
+	commands = map[string]cliCommand{
+		"help": {
+			name:        "help",
+			description: "Displays a help message",
+			callback:    commandHelp,
+		},
+		"exit": {
+			name:        "exit",
+			description: "Exit the Pokedex",
+			callback:    commandExit,
+		},
+	}
+}
+
+func startRepl(commands map[string]cliCommand) {
+	scanner := bufio.NewScanner(os.Stdin)
+	for {
+		fmt.Print("Pokedex > ")
+		scanner.Scan()
+		input := scanner.Text()
+		command := parseFirstWord(input)
+		if cmd, exists := commands[command]; exists {
+			err := cmd.callback()
+			if err != nil {
+				fmt.Println(err)
+			}
+		} else {
+			fmt.Println("Unknown command")
+		}
+	}
 }
